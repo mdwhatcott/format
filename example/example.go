4dd3f8ad27bee1f96793e46331079ed17518704a
Michael Whatcott <mdwhatcott@gmail.com>
2018-02-23 16:51:57 -0700

    Essence.

--- example/example.go
+++ example/example.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 
-	"github.com/mdwhatcott/format"
+	. "github.com/mdwhatcott/format"
 )
 
 var hello, world string
@@ -13,76 +13,75 @@ func main() {
 	print()
 	write()
 	compose()
-
 	read()
 	scan()
 	decompose()
 }
 
 func errors() {
-	format.PrintLine("\nError functions...")
-	format.PrintLine("==================")
-	format.PrintLine(format.Error("This is a '%s, %s!' error.", "Hello", "World"))
+	PrintLine("\nError functions...")
+	PrintLine("==================")
+	PrintLine(Error("This is a '%s, %s!' error.", "Hello", "World"))
 }
 
 func print() {
-	format.PrintLine("\nPrint functions...")
-	format.PrintLine("==================")
-	format.Print("Hello, World!\n")
-	format.PrintFormat("Hello, %s!\n", "World")
-	format.PrintLine("Hello, World!")
+	PrintLine("\nPrint functions...")
+	PrintLine("==================")
+	Print("Hello, World!\n")
+	PrintFormat("Hello, %s!\n", "World")
+	PrintLine("Hello, World!")
 }
 
 func write() {
-	format.PrintLine("\nWrite functions...")
-	format.PrintLine("==================")
+	PrintLine("\nWrite functions...")
+	PrintLine("==================")
 	writer := bytes.NewBufferString("")
-	format.Write(writer, "1")
-	format.WriteFormat(writer, "%d", 2)
-	format.WriteLine(writer, "3")
-	format.Print(writer.String())
+	Write(writer, "1")
+	WriteFormat(writer, "%d", 2)
+	WriteLine(writer, "3")
+	Print(writer.String())
 }
 
 func compose() {
-	format.PrintLine("\nCompose functions...")
-	format.PrintLine("====================")
-	format.Print(format.Compose(1, 2, 3, "\n"))
-	format.Print(format.ComposeFormat("%d %d %d\n", 1, 2, 3))
-	format.Print(format.ComposeLine(1, 2, 3))
+	PrintLine("\nCompose functions...")
+	PrintLine("====================")
+	Print(Compose(1, 2, 3, "\n"))
+	Print(ComposeFormat("%d %d %d\n", 1, 2, 3))
+	Print(ComposeLine(1, 2, 3))
 }
 
 func read() {
-	format.PrintLine("\nRead functions...")
-	format.PrintLine("=================")
-	format.Read(bytes.NewBufferString("Hello, World!"), &hello, &world)
-	format.PrintLine(hello, world)
-	format.ReadFormat(bytes.NewBufferString("Goodbye world"), "%s %s", &hello, &world)
-	format.PrintLine(hello, world)
-	format.ReadLine(bytes.NewBufferString("hi world\n"), &hello, &world)
-	format.PrintLine(hello, world)
+	PrintLine("\nRead functions...")
+	PrintLine("=================")
+	Read(bytes.NewBufferString("Hello, World!"), &hello, &world)
+	PrintLine(hello, world)
+	ReadFormat(bytes.NewBufferString("Goodbye world"), "%s %s", &hello, &world)
+	PrintLine(hello, world)
+	ReadLine(bytes.NewBufferString("hi world\n"), &hello, &world)
+	PrintLine(hello, world)
 }
 
 func scan() {
-	format.PrintLine("\nScan functions... (enter two words, separated by white space, then <ENTER>)")
-	format.PrintLine("=================")
-	format.Print("> ")
-	format.Scan(&hello, &world)
-	format.PrintLine(hello, world)
-	format.Print("> ")
-	format.ScanLine(&hello, &world)
-	format.PrintLine(hello, world)
-	format.Print("> ")
-	format.ScanFormat("%s %s\n", &hello, &world)
-	format.PrintLine(hello, world)
+	PrintLine("\nScan functions... (enter two words, separated by white space, then <ENTER>)")
+	PrintLine("=================")
+	Print("> ")
+	Scan(&hello, &world)
+	PrintLine(hello, world)
+	Print("> ")
+	ScanLine(&hello, &world)
+	PrintLine(hello, world)
+	Print("> ")
+	ScanFormat("%s %s\n", &hello, &world)
+	PrintLine(hello, world)
 }
 
 func decompose() {
-	format.PrintLine("\nDecompose functions...")
-	format.PrintLine("======================")
-	format.Decompose("blah blah", &hello, &world)
-	format.PrintLine(hello, world)
-	format.DecomposeFormat("BLAH BLAH", "%s %s", &hello, &world)
-	format.PrintLine(hello, world)
-	format.DecomposeLine("BlAh bLaH\n", &hello, &world)
-	format.PrintLine(hello, world)
+	PrintLine("\nDecompose functions...")
+	PrintLine("======================")
+	Decompose("blah blah", &hello, &world)
+	PrintLine(hello, world)
+	DecomposeFormat("BLAH BLAH", "%s %s", &hello, &world)
+	PrintLine(hello, world)
+	DecomposeLine("BlAh bLaH\n", &hello, &world)
+	PrintLine(hello, world)
 }
--- format.go
+++ format.go
@@ -2,118 +2,32 @@
 // the AWOL characters, the ambiguities, and the archaic ambiance.
 package format
 
-import (
-	"fmt"
-	"io"
+import "fmt"
+
+var (
+	Error = fmt.Errorf
+
+	Write       = fmt.Fprint
+	WriteFormat = fmt.Fprintf
+	WriteLine   = fmt.Fprintln
+
+	Read       = fmt.Fscan
+	ReadFormat = fmt.Fscanf
+	ReadLine   = fmt.Fscanln
+
+	Print       = fmt.Print
+	PrintFormat = fmt.Printf
+	PrintLine   = fmt.Println
+
+	Scan       = fmt.Scan
+	ScanFormat = fmt.Scanf
+	ScanLine   = fmt.Scanln
+
+	Compose       = fmt.Sprint
+	ComposeFormat = fmt.Sprintf
+	ComposeLine   = fmt.Sprintln
+
+	Decompose       = fmt.Sscan
+	DecomposeFormat = fmt.Sscanf
+	DecomposeLine   = fmt.Sscanln
 )
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Error -> fmt.Errorf
-func Error(format string, items ...interface{}) error {
-	return fmt.Errorf(format, items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Write -> fmt.Fprint
-func Write(writer io.Writer, items ...interface{}) (written int, err error) {
-	return fmt.Fprint(writer, items...)
-}
-
-// WriteFormat -> fmt.Fprintf
-func WriteFormat(writer io.Writer, format string, items ...interface{}) (written int, err error) {
-	return fmt.Fprintf(writer, format, items...)
-}
-
-// WriteLine -> fmt.Fprintln
-func WriteLine(writer io.Writer, items ...interface{}) (written int, err error) {
-	return fmt.Fprintln(writer, items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Read -> fmt.Fscan
-func Read(reader io.Reader, items ...interface{}) (elements int, err error) {
-	return fmt.Fscan(reader, items...)
-}
-
-// ReadFormat -> fmt.Fscanf
-func ReadFormat(reader io.Reader, format string, items ...interface{}) (elements int, err error) {
-	return fmt.Fscanf(reader, format, items...)
-}
-
-// ReadLine -> Fscanln
-func ReadLine(reader io.Reader, items ...interface{}) (elements int, err error) {
-	return fmt.Fscanln(reader, items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Print -> fmt.Print (Hey, what a coincidence!)
-func Print(items ...interface{}) (written int, err error) {
-	return fmt.Print(items...)
-}
-
-// PrintFormat -> fmt.Printf
-func PrintFormat(format string, items ...interface{}) (written int, err error) {
-	return fmt.Printf(format, items...)
-}
-
-// PrintLine -> fmt.Println
-func PrintLine(items ...interface{}) (written int, err error) {
-	return fmt.Println(items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Scan -> fmt.Scan (Hey, what a coincidence!)
-func Scan(items ...interface{}) (elements int, err error) {
-	return fmt.Scan(items...)
-}
-
-// ScanFormat -> fmt.Scanf
-func ScanFormat(format string, items ...interface{}) (elements int, err error) {
-	return fmt.Scanf(format, items...)
-}
-
-// ScanLine -> fmt.Scanln
-func ScanLine(items ...interface{}) (elements int, err error) {
-	return fmt.Scanln(items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Compose -> fmt.Sprint
-func Compose(items ...interface{}) string {
-	return fmt.Sprint(items...)
-}
-
-// ComposeFormat -> fmt.Sprintf
-func ComposeFormat(format string, items ...interface{}) string {
-	return fmt.Sprintf(format, items...)
-}
-
-// ComposeLine -> fmt.Sprintln
-func ComposeLine(items ...interface{}) string {
-	return fmt.Sprintln(items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// Decompose -> fmt.Sscan
-func Decompose(value string, items ...interface{}) (elements int, err error) {
-	return fmt.Sscan(value, items...)
-}
-
-// DecomposeFormat -> fmt.Sscanf
-func DecomposeFormat(value string, format string, items ...interface{}) (elements int, err error) {
-	return fmt.Sscanf(value, format, items...)
-}
-
-// DecomposeLine -> fmt.Sscanln
-func DecomposeLine(value string, items ...interface{}) (elements int, err error) {
-	return fmt.Sscanln(value, items...)
-}
-
-/////////////////////////////////////////////////////////////////////////////////
